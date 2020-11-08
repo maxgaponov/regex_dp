d31024d143489cc6a17518508fbeb48b1cbf4d5e
Max Gaponov <gaponovmaxev@gmail.com>
2020-11-08 22:18:10 +0300

    solver struct

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,14 @@
-int main() {
+#include <solver.h>
+#include <iostream>
 
+void run(std::istream& in, std::ostream& out) {
+    std::string rpn_regex;
+    char x;
+    int k;
+    in >> rpn_regex >> x >> k;
+    out << solve(rpn_regex, x, k) << std::endl;
+}
+
+int main() {
+    run(std::cin, std::cout);
 }
---
+++ src/solver.cpp
@@ -0,0 +1,5 @@
+#include <string>
+
+size_t solve(const std::string rpn_regex, char x, int k) {
+    return 123;
+}
