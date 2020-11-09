f2137baec0b002115b67df02d9920b83168af031
Max Gaponov <gaponovmaxev@gmail.com>
2020-11-09 11:08:24 +0300

    iteration dp_manip

--- src/dynamic_manip.cpp
+++ src/dynamic_manip.cpp
@@ -1,5 +1,6 @@
 #include <dynamic_manip.h>
 #include <algorithm>
+#include <queue>
 
 DynamicState sum(const DynamicState& lhs, const DynamicState& rhs) {
     char x = lhs.get_x();
@@ -29,6 +30,39 @@ DynamicState concat(const DynamicState& lhs, const DynamicState& rhs) {
             }
             if (lhs.is_xword_in_l(i) && rhs.is_xword_in_l(j) && i + j <= k) {
                 result.update_xword_in_l(i + j);
+            }
+        }
+    }
+    return result;
+}
+
+DynamicState iteration(const DynamicState& ds) {
+    char x = ds.get_x();
+    int k = ds.get_k();
+    DynamicState result(x, k);
+    result.update_xword_in_l(0);
+    std::queue<int> q;
+    for (int i = 0; i <= k; ++i) {
+        if (ds.is_xword_in_l(i)) {
+            result.update_xword_in_l(i);
+            q.push(i);
+        }
+    }
+    while (!q.empty()) {
+        int cnt = q.front();
+        q.pop();
+        for (int i = 0; i + cnt <= k; ++i) {
+            if (result.is_xword_in_l(i) && !result.is_xword_in_l(i + cnt)) {
+                result.update_xword_in_l(i + cnt);
+                q.push(i + cnt);
+            }
+        }
+    }
+    for (int i = 0; i <= k; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            if (result.is_xword_in_l(j)) {
+                size_t min_word_len = ds.get_min_word_len(i) + j;
+                result.update_min_word_len(j, min_word_len);
             }
         }
     }
